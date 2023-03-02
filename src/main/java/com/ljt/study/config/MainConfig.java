a52115443a55147633b247791cb8eaca21ab7229
lijtforgithub <lijtforjava@sina.com>
2023-03-02 14:28:01 +0800

    cluster

--- src/main/java/com/ljt/study/config/MainConfig.java
+++ src/main/java/com/ljt/study/config/MainConfig.java
@@ -2,12 +2,7 @@ package com.ljt.study.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
 
 /**
  * @author LiJingTang
@@ -18,13 +13,13 @@ import java.sql.Connection;
 @Configuration
 class MainConfig {
 
-    @Bean
-    ApplicationRunner validateDataSource(DataSource dataSource) {
-        return args -> {
-            log.info("dataSource: {}", dataSource);
-            Connection connection = dataSource.getConnection();
-            log.info("初始化数据库连接池 connection: {}", connection);
-        };
-    }
+//    @Bean
+//    ApplicationRunner validateDataSource(DataSource dataSource) {
+//        return args -> {
+//            log.info("dataSource: {}", dataSource);
+//            Connection connection = dataSource.getConnection();
+//            log.info("初始化数据库连接池 connection: {}", connection);
+//        };
+//    }
 
 }
